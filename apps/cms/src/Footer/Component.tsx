0166a789ed8b249fb5729c1906f7a0859104f2a1
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2026-07-30 11:27:25 -0500

    fix: resolve cms build errors and turborepo cyclic dependency

--- apps/cms/src/Footer/Component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import type { Footer } from '@/payload-types'
-
-export async function Footer() {
-  return (
-    <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white"></footer>
-  )
-}
